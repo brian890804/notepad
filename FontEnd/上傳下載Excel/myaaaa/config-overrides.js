ad6da19f5898fda0f6aedc4b31b59357056f1ab2
brian890804 <71435901+brian890804@users.noreply.github.com>
2022-03-02 11:31:28 +0800

    delete

--- FontEnd/上傳下載Excel/myaaaa/config-overrides.js
+++
@@ -1,8 +0,0 @@
-const { override, addWebpackExternals } = require("customize-cra");
-
-module.exports = override(
-    addWebpackExternals({
-        "./cptable": "var cptable",
-        'dotAll./xlsx.js': 'var_XLSX'
-    })
-)
